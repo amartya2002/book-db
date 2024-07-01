26745c2c60a86246713ea76ba366670f86dde989
Amartya Sinha <amartya@nzminds.com>
2024-07-01 15:04:26 +0530

    Update Dockerfile

--- frontend_service/app/page.jsx
+++ frontend_service/app/page.jsx
@@ -3,6 +3,9 @@ import BooksList from "@/components/booksList";
 export default function Home() {
   return (
     <div className="min-h-screen bg-zinc-100">
+      <div className="bg-white w-full h-16">
+        <a href="/book">Book</a>
+      </div>
       <BooksList />
     </div>
   );
